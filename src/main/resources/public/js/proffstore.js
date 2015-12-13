15cd234302a400d50eaebc40afe38896c7c81026
Dmytro <plekhotkindmytro@gmail.com>
2015-12-13 12:03:34 +0200

    Add a lot of fake data

--- src/main/resources/public/js/proffstore.js
+++ src/main/resources/public/js/proffstore.js
@@ -31,6 +31,53 @@ function drawAll() {
 
 		}
 	});
+	
+	$.ajax({
+		type : 'GET',
+		url : "/static-elance",
+		cache : false,
+		contentType : "application/json; charset=utf-8",
+		dataType : "json",
+		success : function(data) {
+			proffstoreMainChartElance(data);
+
+			$.ajax({
+				type : 'GET',
+				url : "/static-elance-skills",
+				cache : false,
+				contentType : "application/json; charset=utf-8",
+				dataType : "json",
+				success : function(data) {
+					skillsPopularityElance(data);
+				
+				}
+			});
+
+		}
+	});
+	
+	$.ajax({
+		type : 'GET',
+		url : "/freelancer/getAvarageProjectAmount",
+		cache : false,
+		contentType : "application/json; charset=utf-8",
+		dataType : "json",
+		success : function(data) {
+			proffstoreMainChartFreelancer(data);
+
+			$.ajax({
+				type : 'GET',
+				url : "/freelancer/getSkillsPopularity",
+				cache : false,
+				contentType : "application/json; charset=utf-8",
+				dataType : "json",
+				success : function(data) {
+					skillsPopularityFreelancer(data);
+				}
+			});
+
+		}
+	});
 
 }
 
@@ -91,5 +138,125 @@ function skillsPopularity(stats) {
 	};
 	var chart = new google.visualization.PieChart(document
 			.getElementById('skills-popularity'));
+	chart.draw(data, options);
+}
+
+function proffstoreMainChartElance(stats) {
+	var rows = [ [ 'Category', 'Average Budget' ] ];
+
+	var i, len, maxStat = 0;
+	for (i = 0, len = stats.length; i < len; i++) {
+		if (stats[i][1] > maxStat) {
+			maxStat = stats[i][1];
+		}
+	}
+	var ticksArray = [];
+	for (i = 0, len = maxStat / 500; i < len; i++) {
+		ticksArray.push(500 * (i + 1));
+	}
+
+	rows = rows.concat(stats);
+	console.log(stats);
+	var data = google.visualization.arrayToDataTable(rows);
+
+	var options = {
+		title : 'Average budget per category',
+		width : '100%',
+		height : 600,
+		hAxis : {
+			title : 'AVG Budget($)',
+			minValue : 0,
+			ticks : ticksArray
+		},
+		vAxis : {
+			title : 'Category'
+		},
+		chartArea : {
+			left : '40%',
+			width : '60%'
+		}
+	};
+	var chart = new google.visualization.BarChart(document
+			.getElementById('avg-budget-per-category-elance'));
+	chart.draw(data, options);
+}
+
+function skillsPopularityElance(stats) {
+	var rows = [ [ 'Skills', 'Popularity' ] ];
+
+	rows = rows.concat(stats);
+
+	var data = google.visualization.arrayToDataTable(rows);
+
+	var options = {
+		title : 'Skills popularity',
+		width : '100%',
+		height : 600,
+		sliceVisibilityThreshold : .05,
+		pieResidueSliceLabel : "Other",
+		//pieHole : 0.4
+	};
+	var chart = new google.visualization.PieChart(document
+			.getElementById('skills-popularity-elance'));
+	chart.draw(data, options);
+}
+
+function proffstoreMainChartFreelancer(stats) {
+	var rows = [ [ 'Category', 'Average Budget' ] ];
+
+	var i, len, maxStat = 0;
+	for (i = 0, len = stats.length; i < len; i++) {
+		if (stats[i][1] > maxStat) {
+			maxStat = stats[i][1];
+		}
+	}
+	var ticksArray = [];
+	for (i = 0, len = maxStat / 1000; i < len; i++) {
+		ticksArray.push(1000 * (i + 1));
+	}
+
+	rows = rows.concat(stats);
+	console.log(stats);
+	var data = google.visualization.arrayToDataTable(rows);
+
+	var options = {
+		title : 'Average budget per category',
+		width : '100%',
+		height : 600,
+		hAxis : {
+			title : 'AVG Budget($)',
+			minValue : 0,
+			ticks : ticksArray
+		},
+		vAxis : {
+			title : 'Category'
+		},
+		chartArea : {
+			left : '40%',
+			width : '60%'
+		}
+	};
+	var chart = new google.visualization.BarChart(document
+			.getElementById('avg-budget-per-category-freelancer'));
+	chart.draw(data, options);
+}
+
+function skillsPopularityFreelancer(stats) {
+	var rows = [ [ 'Skills', 'Popularity' ] ];
+
+	rows = rows.concat(stats);
+
+	var data = google.visualization.arrayToDataTable(rows);
+
+	var options = {
+		title : 'Skills popularity',
+		width : '100%',
+		height : 600,
+		sliceVisibilityThreshold : .05,
+		pieResidueSliceLabel : "Other",
+		//pieHole : 0.4
+	};
+	var chart = new google.visualization.PieChart(document
+			.getElementById('skills-popularity-freelancer'));
 	chart.draw(data, options);
 }
