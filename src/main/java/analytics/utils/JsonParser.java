d47fe0857af3131e5e3c7faf95e14beada256118
Nick Barban <nn.barban@gmail.com>
2015-12-13 08:10:53 +0200

    Add Json Parser, Update version of Jsoup dependency

---
+++ src/main/java/analytics/utils/JsonParser.java
@@ -0,0 +1,81 @@
+package analytics.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JsonParser {
+
+	private final static Pattern p = Pattern.compile("-?\\d+");
+
+	public static List<String> getCategories(JSONObject jsonObject) throws JSONException {
+		List<String> result = new ArrayList<>();
+		for (int i = 0; i < jsonObject.getJSONObject("data").getJSONArray("pageResults").length(); i++) {
+			String categoryName = jsonObject.getJSONObject("data").getJSONArray("pageResults").getJSONObject(i)
+					.getString("category");
+			result.add(categoryName);
+		}
+		return result;
+	}
+
+	public static Map<String, Double> getAverageBudgetPerCategory(List<String> categories, JSONObject jsonObject)
+			throws JSONException {
+		Map<String, Double> result = new HashMap<>();
+		for (String etalon : categories) {
+			double averageBudget = 0;
+			int count = 0;
+			for (int i = 0; i < jsonObject.getJSONObject("data").getJSONArray("pageResults").length(); i++) {
+
+				String categoryName;
+				try {
+					categoryName = jsonObject.getJSONObject("data").getJSONArray("pageResults").getJSONObject(i)
+							.getString("category");
+					if (categoryName.equalsIgnoreCase(etalon)) {
+						double budget = getBudget(
+								jsonObject.getJSONObject("data").getJSONArray("pageResults").getJSONObject(i));
+						if (budget > 0) {
+							count++;
+						}
+						averageBudget += budget;
+					}
+				} catch (Exception e) {
+					continue;
+				}
+
+			}
+			averageBudget /= count;
+			result.put(etalon, averageBudget);
+		}
+		return result;
+	}
+
+	private static double getBudget(JSONObject jsonObject) throws JSONException {
+		double result = 0;
+		try {
+			result = Double.parseDouble(jsonObject.getString("budget"));
+		} catch (NumberFormatException e) {
+			result = getAverage(jsonObject.getString("budget"));
+		}
+		return result;
+	}
+
+	private static double getAverage(String string) {
+		List<Double> numbers = new ArrayList<>();
+		Matcher m = p.matcher(string);
+		while (m.find()) {
+			try {
+				numbers.add(Double.parseDouble(m.group()));
+			} catch (NumberFormatException e) {
+				continue;
+			}
+		}
+		return numbers.size() > 0 ? numbers.stream().mapToDouble(d -> 1 * d).sum() / numbers.size() : 0d;
+	}
+
+}
